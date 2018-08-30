e1eaa200d4e1badc95eff252c2263ea23d987412
Jeffrey Miller <millerjl1701@gmail.com>
2018-08-30 09:54:25 -0500

    renamed rspec/acceptance class files

---
+++ spec/classes/tigervnc_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'tigervnc' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context "tigervnc class without any parameters changed from defaults" do
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_class('tigervnc::install') }
+          it { is_expected.to contain_class('tigervnc::config') }
+          it { is_expected.to contain_class('tigervnc::service') }
+          it { is_expected.to contain_class('tigervnc::install').that_comes_before('Class[tigervnc::config]') }
+          it { is_expected.to contain_class('tigervnc::service').that_subscribes_to('Class[tigervnc::config]') }
+
+          it { is_expected.to contain_package('tigervnc-server').with_ensure('present') }
+
+          it { is_expected.to contain_service('vncserver').with(
+            'ensure'     => 'running',
+            'enable'     => 'true',
+            'hasstatus'  => 'true',
+            'hasrestart' => 'true',
+          ) }
+        end
+      end
+    end
+  end
+
+  context 'unsupported operating system' do
+    describe 'tigervnc class without any parameters on Solaris/Nexenta' do
+      let(:facts) do
+        {
+          :osfamily        => 'Solaris',
+          :operatingsystem => 'Nexenta',
+        }
+      end
+
+      it { expect { is_expected.to contain_package('tigervnc') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+    end
+  end
+end
